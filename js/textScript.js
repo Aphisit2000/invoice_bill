74d3adfcfc6d7bbc23bb9976816d1175d118c902
RED_CHINA <112806554+Aphisit2000@users.noreply.github.com>
2023-03-29 15:36:18 +0700

    add invoice_bill

---
+++ js/textScript.js
@@ -0,0 +1,323 @@
+function copyText() {
+  var textbox1 = document.getElementById("number");
+  var textbox2 = document.getElementById("number-c");
+  textbox2.value = textbox1.value;
+
+  var textbox3 = document.getElementById("numberc1");
+  var textbox4 = document.getElementById("number-c1");
+  textbox4.value = textbox3.value;
+
+  var textbox5 = document.getElementById("numberc1");
+  var textbox6 = document.getElementById("number-c1");
+  textbox6.value = textbox5.value;
+
+  var textbox7 = document.getElementById("numberc2");
+  var textbox8 = document.getElementById("number-c2");
+  textbox8.value = textbox7.value;
+
+  var textbox9 = document.getElementById("numberc3");
+  var textbox10 = document.getElementById("number-c3");
+  textbox10.value = textbox9.value;
+
+  var textbox11 = document.getElementById("numberc4");
+  var textbox12 = document.getElementById("number-c4");
+  textbox12.value = textbox11.value;
+
+  var textbox13 = document.getElementById("numberc5");
+  var textbox14 = document.getElementById("number-c5");
+  textbox14.value = textbox13.value;
+
+  var textbox15 = document.getElementById("numberc6");
+  var textbox16 = document.getElementById("number-c6");
+  textbox16.value = textbox15.value;
+
+  var textbox17 = document.getElementById("numberc7");
+  var textbox18 = document.getElementById("number-c7");
+  textbox18.value = textbox17.value;
+
+  var textbox19 = document.getElementById("numberc8");
+  var textbox20 = document.getElementById("number-c8");
+  textbox20.value = textbox19.value;
+
+  var textbox21 = document.getElementById("numberc9");
+  var textbox22 = document.getElementById("number-c9");
+  textbox22.value = textbox21.value;
+
+  var textboxi1 = document.getElementById("info");
+  var textboxi2 = document.getElementById("info-c");
+  textboxi2.value = textboxi1.value;
+
+  var textboxi3 = document.getElementById("info1");
+  var textboxi4 = document.getElementById("info-c1");
+  textboxi4.value = textboxi3.value;
+
+  var textboxi5 = document.getElementById("info2");
+  var textboxi6 = document.getElementById("info-c2");
+  textboxi6.value = textboxi5.value;
+
+  var textboxi7 = document.getElementById("info3");
+  var textboxi8 = document.getElementById("info-c3");
+  textboxi8.value = textboxi7.value;
+
+  var textboxi9 = document.getElementById("info4");
+  var textboxi10 = document.getElementById("info-c4");
+  textboxi10.value = textboxi9.value;
+
+  var textboxi11 = document.getElementById("info5");
+  var textboxi12 = document.getElementById("info-c5");
+  textboxi12.value = textboxi11.value;
+
+  var textboxi13 = document.getElementById("info6");
+  var textboxi14 = document.getElementById("info-c6");
+  textboxi14.value = textboxi13.value;
+
+  var textboxi15 = document.getElementById("info7");
+  var textboxi16 = document.getElementById("info-c7");
+  textboxi16.value = textboxi15.value;
+
+  var textboxi17 = document.getElementById("info8");
+  var textboxi18 = document.getElementById("info-c8");
+  textboxi18.value = textboxi17.value;
+
+  var textboxi19 = document.getElementById("info9");
+  var textboxi20 = document.getElementById("info-c9");
+  textboxi20.value = textboxi19.value;
+
+  var textboxa1 = document.getElementById("number2");
+  var textboxa2 = document.getElementById("number2c");
+  textboxa2.value = textboxa1.value;
+
+  var textboxa3 = document.getElementById("number2-1");
+  var textboxa4 = document.getElementById("number2-1c");
+  textboxa4.value = textboxa3.value;
+
+  var textboxa5 = document.getElementById("number2-2");
+  var textboxa6 = document.getElementById("number2-2c");
+  textboxa6.value = textboxa5.value;
+
+  var textboxa7 = document.getElementById("number2-3");
+  var textboxa8 = document.getElementById("number2-3c");
+  textboxa8.value = textboxa7.value;
+
+  var textboxa74 = document.getElementById("number2-4");
+  var textboxa84 = document.getElementById("number2-4c");
+  textboxa84.value = textboxa74.value;
+
+  var textboxa11 = document.getElementById("number2-5");
+  var textboxa12 = document.getElementById("number2-5c");
+  textboxa12.value = textboxa11.value;
+
+  var textboxa13 = document.getElementById("number2-6");
+  var textboxa14 = document.getElementById("number2-6c");
+  textboxa14.value = textboxa13.value;
+
+  var textboxa15 = document.getElementById("number2-7");
+  var textboxa16 = document.getElementById("number2-7c");
+  textboxa16.value = textboxa15.value;
+
+  var textbox17 = document.getElementById("number2-8");
+  var textbox18 = document.getElementById("number2-8c");
+  textbox18.value = textbox17.value;
+
+  var textboxa19 = document.getElementById("number2-9");
+  var textboxa20 = document.getElementById("number2-9c");
+  textboxa20.value = textboxa19.value;
+
+  var textboxa1a = document.getElementById("number3");
+  var textboxa2a = document.getElementById("number3c");
+  textboxa2a.value = textboxa1a.value;
+
+  var textboxa3a = document.getElementById("number3-1");
+  var textboxa4a = document.getElementById("number3-1c");
+  textboxa4a.value = textboxa3a.value;
+
+  var textboxa5a = document.getElementById("number3-2");
+  var textboxa6a = document.getElementById("number3-2c");
+  textboxa6a.value = textboxa5a.value;
+
+  var textboxa7a = document.getElementById("number3-3");
+  var textboxa8a = document.getElementById("number3-3c");
+  textboxa8a.value = textboxa7a.value;
+
+  var textboxa74a = document.getElementById("number3-4");
+  var textboxa84a = document.getElementById("number3-4c");
+  textboxa84a.value = textboxa74a.value;
+
+  var textboxa11a = document.getElementById("number3-5");
+  var textboxa12a = document.getElementById("number3-5c");
+  textboxa12a.value = textboxa11a.value;
+
+  var textboxa13a = document.getElementById("number3-6");
+  var textboxa14a = document.getElementById("number3-6c");
+  textboxa14a.value = textboxa13a.value;
+
+  var textboxa13aa = document.getElementById("number3-7");
+  var textboxa14aa = document.getElementById("number3-7c");
+  textboxa14aa.value = textboxa13aa.value;
+
+  var textbox17a = document.getElementById("number3-8");
+  var textbox18a = document.getElementById("number3-8c");
+  textbox18a.value = textbox17a.value;
+
+  var textboxa19a = document.getElementById("number3-9");
+  var textboxa20a = document.getElementById("number3-9c");
+  textboxa20a.value = textboxa19a.value;
+
+  var text1 = document.getElementById("number4");
+  var text2 = document.getElementById("number4c");
+  text2.value = text1.value;
+
+  var text3 = document.getElementById("number4-1");
+  var text4 = document.getElementById("number4-1c");
+  text4.value = text3.value;
+
+  var text5 = document.getElementById("number4-2");
+  var text6 = document.getElementById("number4-2c");
+  text6.value = text5.value;
+
+  var text7 = document.getElementById("number4-3");
+  var text8 = document.getElementById("number4-3c");
+  text8.value = text7.value;
+
+  var text9 = document.getElementById("number4-4");
+  var text10 = document.getElementById("number4-4c");
+  text10.value = text9.value;
+
+  var text11 = document.getElementById("number4-5");
+  var text12 = document.getElementById("number4-5c");
+  text12.value = text11.value;
+
+  var text13 = document.getElementById("number4-6");
+  var text14 = document.getElementById("number4-6c");
+  text14.value = text13.value;
+
+  var text15 = document.getElementById("number4-7");
+  var text16 = document.getElementById("number4-7c");
+  text16.value = text15.value;
+
+  var text17 = document.getElementById("number4-8");
+  var text18 = document.getElementById("number4-8c");
+  text18.value = text17.value;
+
+  var text19 = document.getElementById("number4-9");
+  var text20 = document.getElementById("number4-9c");
+  text20.value = text19.value;
+
+  var text1a = document.getElementById("number5");
+  var text2a = document.getElementById("number5c");
+  text2a.value = text1a.value;
+
+  var text3a = document.getElementById("number5-1");
+  var text4a = document.getElementById("number5-1c");
+  text4a.value = text3a.value;
+
+  var text5a = document.getElementById("number5-2");
+  var text6a = document.getElementById("number5-2c");
+  text6a.value = text5a.value;
+
+  var text7a = document.getElementById("number5-3");
+  var text8a = document.getElementById("number5-3c");
+  text8a.value = text7a.value;
+
+  var text9a = document.getElementById("number5-4");
+  var text10a = document.getElementById("number5-4c");
+  text10a.value = text9a.value;
+
+  var text11a = document.getElementById("number5-5");
+  var text12a = document.getElementById("number5-5c");
+  text12a.value = text11a.value;
+
+  var text13a = document.getElementById("number5-6");
+  var text14a = document.getElementById("number5-6c");
+  text14a.value = text13a.value;
+
+  var text15a = document.getElementById("number5-7");
+  var text16a = document.getElementById("number5-7c");
+  text16a.value = text15a.value;
+
+  var text17a = document.getElementById("number5-8");
+  var text18a = document.getElementById("number5-8c");
+  text18a.value = text17a.value;
+
+  var text19a = document.getElementById("number5-9");
+  var text20a = document.getElementById("number5-9c");
+  text20a.value = text19a.value;
+
+  var text1b = document.getElementById("pic1");
+  var text2b = document.getElementById("pic1c");
+  text2b.value = text1b.value;
+
+  var text3b = document.getElementById("pic2");
+  var text4b = document.getElementById("pic2c");
+  text4b.value = text3b.value;
+
+  var text5b = document.getElementById("pic3");
+  var text6b = document.getElementById("pic3c");
+  text6b.value = text5b.value;
+
+  var text1c = document.getElementById("n1");
+  var text2c = document.getElementById("n1c");
+  text2c.value = text1c.value;
+
+  var text3c = document.getElementById("n2");
+  var text4c = document.getElementById("n2c");
+  text4c.value = text3c.value;
+
+  var text5c = document.getElementById("n3");
+  var text6c = document.getElementById("n3c");
+  text6c.value = text5c.value;
+
+  var text7c = document.getElementById("n4");
+  var text8c = document.getElementById("n4c");
+  text8c.value = text7c.value;
+
+  var text9c = document.getElementById("n5");
+  var text10c = document.getElementById("n5c");
+  text10c.value = text9c.value;
+
+  var text11c = document.getElementById("n6");
+  var text12c = document.getElementById("n6c");
+  text12c.value = text11c.value;
+
+  var text13c = document.getElementById("n7");
+  var text14c = document.getElementById("n7c");
+  text14c.value = text13c.value;
+
+  var text15c = document.getElementById("datetime");
+  var text16c = document.getElementById("datetimec");
+  text16c.value = text15c.value;
+
+  var number5 = parseInt(document.getElementById("number5").value) || 0;
+  var number4 = parseInt(document.getElementById("number5-1").value) || 0;
+  var number3 = parseInt(document.getElementById("number5-2").value) || 0;
+  var number2 = parseInt(document.getElementById("number5-3").value) || 0;
+  var number1 = parseInt(document.getElementById("number5-4").value) || 0;
+  var number0 = parseInt(document.getElementById("number5-5").value) || 0;
+  var numberNeg1 = parseInt(document.getElementById("number5-6").value) || 0;
+  var numberNeg2 = parseInt(document.getElementById("number5-7").value) || 0;
+  var numberNeg3 = parseInt(document.getElementById("number5-8").value) || 0;
+  var numberNeg4 = parseInt(document.getElementById("number5-9").value) || 0;
+
+  var pic2 = parseInt(document.getElementById("pic2").value) || 0;
+
+  var sum =
+    number5 +
+    number4 +
+    number3 +
+    number2 +
+    number1 +
+    number0 +
+    numberNeg1 +
+    numberNeg2 +
+    numberNeg3 +
+    numberNeg4 +
+    pic2;
+  var pic3 = document.getElementById("pic3");
+  pic3.value = sum;
+  var pic1 = document.getElementById("pic1");
+  pic1.value = sum;
+  var pic3c = document.getElementById("pic3c");
+  pic3c.value = sum;
+  var pic1c = document.getElementById("pic1c");
+  pic1c.value = sum;
+}
